ad8035c77727184b4026c97376096576a9993a9f
LushiLiu57 <lushiliu1@gmail.com>
2023-09-14 18:22:32 -0400

    added multiple about me cards

--- portfolio-jsm/src/components/AboutMe.tsx
+++ portfolio-jsm/src/components/AboutMe.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ReviewCard from "./ReviewCard";
 import { ArrowLeft, ArrowRight } from "iconsax-react";
 
 const AboutMe = () => {
+  const [currCard, setCurrCard] = useState(1);
+
+  const handlePrevCard = () => {
+    currCard === 1 ? setCurrCard(3) : setCurrCard(currCard - 1);
+  };
+  const handleNextCard = () => {
+    currCard === 3 ? setCurrCard(1) : setCurrCard(currCard + 1);
+  };
+
   return (
     <main className="mt-8 w-full bg-white-900 dark:bg-black-200 md:mt-16">
       <div className="flex w-full flex-col items-center text-black-400 md:h-[100%] md:w-[100%]">
@@ -10,24 +20,45 @@ const AboutMe = () => {
           What they say about me
         </h3>
         <div className="flex flex-row md:mb-16">
-          <div className="mr-6 mt-36 hidden md:flex">
+          <div className="mr-6 mt-36 hidden md:flex" onClick={handlePrevCard}>
             <ArrowLeft />
           </div>
-          <ReviewCard
-            image="./sun.svg"
-            name="Louis Liu"
-            title="Founder & CEO at JS Mastery"
-            desc="I have had the pleasure of working with Adrian as a Software Developer and can confidently 
+          {currCard === 1 ? (
+            <ReviewCard
+              image="./sun.svg"
+              name="Louis Liu"
+              title="Founder & CEO at JS Mastery"
+              desc="I have had the pleasure of working with Adrian as a Software Developer and can confidently 
           say that he is a valuable asset to any team. He is highly skilled in ReactJS, NextJS, Javascript, 
           Material UI, Tailwind CSS, Redux, and GitHub, and consistently delivers high-quality code. "
-          />
+            />
+          ) : currCard === 2 ? (
+            <ReviewCard
+              image="./moon.svg"
+              name="Louis Liu"
+              title="Founder & CEO at JS Mastery"
+              desc="I have had the pleasure of working with Adrian as a Software Developer and can confidently 
+        say that he is a valuable asset to any team. He is highly skilled in ReactJS, NextJS, Javascript, 
+        Material UI, Tailwind CSS, Redux, and GitHub, and consistently delivers high-quality code. "
+            />
+          ) : (
+            <ReviewCard
+              image="./react.svg"
+              name="Louis Liu"
+              title="Founder & CEO at JS Mastery"
+              desc="I have had the pleasure of working with Adrian as a Software Developer and can confidently 
+      say that he is a valuable asset to any team. He is highly skilled in ReactJS, NextJS, Javascript, 
+      Material UI, Tailwind CSS, Redux, and GitHub, and consistently delivers high-quality code. "
+            />
+          )}
+
           <div className="ml-6 mt-36 hidden md:flex">
-            <ArrowRight />
+            <ArrowRight onClick={handleNextCard} />
           </div>
         </div>
         <div className="flex flex-row gap-10 md:hidden">
-          <ArrowLeft />
-          <ArrowRight />
+          <ArrowLeft onClick={handlePrevCard} />
+          <ArrowRight onClick={handleNextCard} />
         </div>
       </div>
     </main>
