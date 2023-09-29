5f85fd5eea8d6bfc46545d8e07b0f0ea27b89e00
LushiLiu57 <lushiliu1@gmail.com>
2023-09-28 22:30:38 -0400

    Finished up Education section

--- portfolio-jsm/src/components/Education.tsx
+++ portfolio-jsm/src/components/Education.tsx
@@ -1,18 +1,49 @@
 import React from "react";
+import SchoolIcon from "@mui/icons-material/School";
+import Image from "next/image";
 
 const Education = () => {
   return (
     <main className="mt-8 w-full bg-white-900 dark:bg-black-200 md:mt-16">
       <div className="flex flex-col md:my-[72px] md:flex-row ">
-        <div className="rounded-[10px] bg-black-200 text-white-800 dark:bg-white-800 dark:text-black-200 md:ml-[85px] md:h-[593px] md:w-[40%]">
-          <h1 className="font-extrabold md:ml-[108px] md:mt-[152px] md:text-48">
-            Education
+        <div className="ml-4 h-[340px] w-[345px] rounded-[10px] bg-black-200 text-white-800 dark:bg-white-800 dark:text-black-200 md:ml-[85px] md:h-[593px] md:w-[40%]">
+          <h1 className="ml-[10%] mt-[30%] text-20 font-extrabold md:ml-[108px] md:mt-[152px] md:text-48">
+            Education & Work Experience
           </h1>
-          <h3 className="md:ml-[108px] md:mt-8 md:text-18">
-            An overview of my educational background
+          <h3 className="ml-[10%] mt-3 md:ml-[108px] md:mt-8 md:text-18">
+            An overview of my educational background and work experience
           </h3>
         </div>
-        <div className="ml-16">Education: University of Toronto</div>
+        <ul className="ml-16">
+          <li className="flex flex-col md:flex-row md:gap-16">
+            <SchoolIcon className="h-20 w-20" />
+            <div className="flex flex-col">
+              <h1 className="text-32 font-bold md:text-48">Education</h1>
+              <h3 className="text-24 font-bold text-blue-light dark:text-blue-dark md:text-32">
+                University of Toronto
+              </h3>
+              <h3>2015-2020</h3>
+              <h3 className="text-24 font-bold md:text-32">
+                Machine Learning and Data Science
+              </h3>
+            </div>
+          </li>
+          <li className="mt-8 flex flex-col md:mt-16 md:flex-row md:gap-16">
+            <Image src="/js.svg" alt="js" width={80} height={80} />
+            <div className="flex flex-col">
+              <h1 className="text-32 font-bold md:mt-20 md:text-48">
+                Education
+              </h1>
+              <h3 className="text-24 font-bold text-blue-light dark:text-blue-dark md:text-32">
+                JavaScript Mastery
+              </h3>
+              <h3>2023-2023</h3>
+              <h3 className="text-24 font-bold md:text-32">
+                Online course on React and Next.js
+              </h3>
+            </div>
+          </li>
+        </ul>
       </div>
     </main>
   );
