0663575324ead8e280642ec398c7d899f6e43df5
Lushi Liu <lushiliu1@gmail.com>
2025-04-08 01:40:22 -0400

    changed get in touch button

--- portfolio-jsm/src/components/GetInTouch.tsx
+++ portfolio-jsm/src/components/GetInTouch.tsx
@@ -8,9 +8,9 @@ import Link from "next/link";
 const GetInTouch = () => {
   const isMobile = useMediaQuery({ maxWidth: 1440 });
   return (
-    <main className="bg-yellow-primary mt-10 h-[311px] w-[90%] items-center rounded-[10px] text-center md:h-[330px]">
+    <main className="mt-10 h-[311px] w-[90%] items-center rounded-[10px] bg-yellow-primary text-center md:h-[330px]">
       <div className="flex flex-col justify-between md:flex-row">
-        <h1 className=" text-black-200 md:text-48 mt-20 font-bold md:ml-16 md:mt-12 md:w-[520px]">
+        <h1 className=" mt-20 font-bold text-black-200 md:ml-16 md:mt-12 md:w-[520px] md:text-48">
           Have a project in mind that requires technical expertise?
         </h1>
         <Link href="/contact" className="mt-2 md:mr-16 md:mt-44">
@@ -24,8 +24,8 @@ const GetInTouch = () => {
             onClick={() => {}}
             text={
               <>
-                Get in Touch with Med
-                <ArrowRight className="text-18 text-white-900 ml-3 inline" />
+                Get in Touch with Me!
+                <ArrowRight className="ml-3 inline text-18 text-white-900" />
               </>
             }
           />
